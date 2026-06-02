bd2202f1e7988a9bb6af7e2d1cf27e9be902e262
Ardhiyonda <dhiovino06@gmail.com>
2026-06-02 16:55:04 +0700

    Perbaikan Broken Build

--- tests/CatalogTest.php
+++ tests/CatalogTest.php
@@ -34,7 +34,7 @@ class CatalogTest extends TestCase
     {
         $result = $this->catalog->searchProduct("Kemeja");
 
-        $this->assertCount(99, $result);
+        $this->assertCount(1, $result);
     }
 
     public function testSearchProductEmptyKeyword()
